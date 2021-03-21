f1f3f1803f83b7caedeb732e93a6a846b976a8c6
Junsu <murane@naver.com>
2021-03-21 21:58:35 +0900

    feat:Auth:Dto > Auth Dto 추가

---
+++ src/main/java/com/example/demo/user/dto/AuthDto.java
@@ -0,0 +1,50 @@
+package com.example.demo.user.dto;
+
+import com.example.demo.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class AuthDto {
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Request{
+        private String login_id;
+        private String password;
+        private String name;
+
+        @Builder
+        Request(String login_id, String password, String name){
+            this.login_id = login_id;
+            this.password = password;
+            this.name = name;
+        }
+
+        public User toEntity(AuthDto.Request reqDto){
+            return User.builder()
+                    .login_id("id")
+                    .password("pw")
+                    .name("nm")
+                    .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response{
+        private Long id;
+        private String login_id;
+        private String password;
+        private String name;
+
+        @Builder
+        Response(Long id, String login_id, String password, String name){
+            this.id=id;
+            this.login_id = login_id;
+            this.password = password;
+            this.name = name;
+        }
+
+    }
+}
