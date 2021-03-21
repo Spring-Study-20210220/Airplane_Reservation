9fa4d376a5ac5f66f14df6d4f9966c8b9bb087d1
Junsu <murane@naver.com>
2021-03-21 21:58:00 +0900

    feat:User:User > User빌더 추가

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -26,4 +26,11 @@ public abstract class User {
     @Column(nullable = false)
     protected String name;
 
+    @Builder
+    User(String login_id, String password, String name) {
+        this.login_id = login_id;
+        this.name = name;
+        this.password = password;
+    }
+
 }
