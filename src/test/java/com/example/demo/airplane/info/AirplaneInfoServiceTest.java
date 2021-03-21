21c0e94439413f179262e3499dfb513277c127e3
peachberry <peachberry0318@gmail.com>
2021-03-21 09:32:24 +0900

    feat(AirplaneInfoServiceTest) : 서비스 save 테스트 추가

---
+++ src/test/java/com/example/demo/airplane/info/AirplaneInfoServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.airplane.info;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class AirplaneInfoServiceTest {
+
+    @Test
+    @DisplayName("비행기 정보가 제대로 저장되는지 테스트")
+    void saveAirplaneInfoTest() {
+
+        given()
+    }
+}
