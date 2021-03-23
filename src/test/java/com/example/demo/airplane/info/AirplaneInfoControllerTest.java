85883199dae994ff43d385a68dda50b0587a48a0
peachberry <peachberry0318@gmail.com>
2021-03-23 13:50:14 +0900

    feat(AirplaneInfoTest) : AirplaneInfo 도메인 Save 기능 테스트

--- src/test/java/com/example/demo/airplane/info/AirplaneInfoControllerTest.java
+++ src/test/java/com/example/demo/airplane/info/AirplaneInfoControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.airplane.info;
 
-import com.example.demo.airplane.info.dto.ControllerRequest;
+import com.example.demo.airplane.info.dto.Request;
 import com.example.demo.airplane.info.dto.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +19,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,10 +45,10 @@ public class AirplaneInfoControllerTest {
         LocalTime time = LocalTime.now();
         LocalDate date = LocalDate.now();
 
-        ControllerRequest.Save save = new ControllerRequest.Save(departure, arrival, date, time);
+        Request.Save save = new Request.Save(departure, arrival, date, time);
         String request = objectMapper.writeValueAsString(save);
 
-        given(airplaneInfoService.save(any())).willReturn(new Response.Save(1L));
+        given(airplaneInfoService.save(any())).willReturn(1L);
 
         mockMvc.perform(post("/airplaneinfo/save")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -59,12 +60,27 @@ public class AirplaneInfoControllerTest {
         verify(airplaneInfoService, times(1)).save(any());
     }
 
+
     @Test
     @DisplayName("비행기 정보 업데이트")
-    void updateAirplaneInfo() {
+    void updateAirplaneInfo() throws Exception {
 
+        Request.Update update = new Request.Update();
+        String request = objectMapper.writeValueAsString(update);
+
+        given(airplaneInfoService.update(any())).willReturn(1L);
+
+        mockMvc.perform(put("/airplaneinfo/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L));
+
+        verify(airplaneInfoService, times(1)).update(any());
     }
 
+/*
     @Test
     @DisplayName("비행기 정보 가져오기")
     void getAirplaneInfo() {
@@ -76,5 +92,7 @@ public class AirplaneInfoControllerTest {
     void getAirplaneInfoList() {
 
     }
+
+ */
 
 }
--- src/test/java/com/example/demo/airplane/info/AirplaneInfoServiceTest.java
+++ src/test/java/com/example/demo/airplane/info/AirplaneInfoServiceTest.java
@@ -1,19 +1,48 @@
 package com.example.demo.airplane.info;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class AirplaneInfoServiceTest {
+
+    @Mock
+    private AirplaneInfoRepository airplaneInfoRepository;
+
+    @InjectMocks
+    private AirplaneInfoService airplaneInfoService;
 
     @Test
     @DisplayName("비행기 정보가 제대로 저장되는지 테스트")
     void saveAirplaneInfoTest() {
 
-        given()
+        AirplaneInfo airplaneInfo = AirplaneInfo
+                .builder()
+                .id(1L)
+                .departure("서울")
+                .arrival("워싱턴")
+                .takeOffDate(LocalDate.now())
+                .takeOffTime(LocalTime.now())
+                .build();
+
+        given(airplaneInfoRepository.save(any())).willReturn(airplaneInfo);
+
+        Long result = airplaneInfoService.save(airplaneInfo);
+
+        Assertions.assertThat(result).isEqualTo(airplaneInfo.getId());
+        verify(airplaneInfoRepository, times(1)).save(any());
     }
 }
