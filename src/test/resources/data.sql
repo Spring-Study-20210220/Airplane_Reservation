442fc5df38f1bc4f493046552e683a93837f4c0b
peachberry <peachberry0318@gmail.com>
2021-03-26 10:37:58 +0900

    feat(resources) : DB초기화 하는 data.sql 작성

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+insert into airplane_info(departure, arrival, take_off_date, take_off_time) values ('Seoul', 'Busan', '2021-01-02', '11:11:00');
+insert into airplane_info(departure, arrival, take_off_date, take_off_time) values ('Seoul2', 'Busan2', '2021-01-02', '11:11:00');
+insert into airplane_info(departure, arrival, take_off_date, take_off_time) values ('Seoul3', 'Busan3', '2021-01-02', '11:11:00');
+insert into airplane_info(departure, arrival, take_off_date, take_off_time) values ('Seoul4', 'Busan4', '2021-01-02', '11:11:00');
