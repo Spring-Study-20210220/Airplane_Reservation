53f409d482bffce6c537ebecce1f170573f3f00e
peachberry <peachberry0318@gmail.com>
2021-03-27 15:06:30 +0900

    refactor(AirplaneInfo) : AirplaneInfo 도메인 -> Airplane으로 변경

--- src/main/java/com/example/demo/airplane/info/AirplaneInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.airplane.info;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirplaneInfoRepository extends JpaRepository<AirplaneInfo, Long> {
-
-}
