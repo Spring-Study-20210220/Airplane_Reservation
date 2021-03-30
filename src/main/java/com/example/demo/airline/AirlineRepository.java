072fa9b1bfa44718d473ab1cf08955419de0d779
Junsu <murane@naver.com>
2021-03-30 22:53:27 +0900

    feat:AirlineRepository > findByName 추가

--- src/main/java/com/example/demo/airline/AirlineRepository.java
+++ src/main/java/com/example/demo/airline/AirlineRepository.java
@@ -2,5 +2,8 @@ package com.example.demo.airline;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AirlineRepository extends JpaRepository<Airline, Long> {
+    Optional<Airline> findByName(String name);
 }
