2f0c4f26c1ffb909b706c8e38112ea8c2a64bd6b
Junsu <murane@naver.com>
2021-04-01 19:56:57 +0900

    refactor:AdminAuthorizeService > 인증 주체 변경

--- src/main/java/com/example/demo/airplain/AirplaneController.java
+++ src/main/java/com/example/demo/airplain/AirplaneController.java
@@ -1,13 +1,28 @@
 package com.example.demo.airplain;
 
+import com.example.demo.airline.dto.AirlineDto;
+import com.example.demo.airplain.dto.AirplaneDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AirplaneController {
     private final AirplainService airplainService;
+
+    @PostMapping("/Airplane")
+    ResponseEntity<AirplaneDto.Response> register(@RequestBody AirplaneDto.Request reqDto
+            , @RequestHeader(value = "Authorization") String authorization) {
+        AirlineDto.Response resDto = airplainService.airplaneCreate(reqDto, authorization);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(resDto);
+    }
+
+
 }
